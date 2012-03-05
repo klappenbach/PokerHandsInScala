6d8d7c553717c74c88b4abfbeeecfe135c390884
Gabriel Klappenbach <gabriel.klappenbach@gmail.com>
2012-03-05 22:32:06 +0100

    refactored cards to a list with highest value first

--- src/main/scala/Card.scala
+++ src/main/scala/Card.scala
@@ -3,6 +3,9 @@ import Suite._
 
 case class Card(rank: Rank, suite: Suite) extends Ordered[Card] {
 
+  /**
+   * Compares rank first, and if same rank compare suite.
+   */
   def compare(that: Card) = rank compare that.rank match {
     case 0 => suite.compare(that.suite)
     case x => x
--- src/main/scala/Hand.scala
+++ src/main/scala/Hand.scala
@@ -2,13 +2,13 @@ import collection.immutable.SortedSet
 
 case class Hand(card1: Card, card2: Card, card3: Card, card4: Card, card5: Card) {
   val cards = try {
-    SortedSet(card1, card2, card3, card4, card5)
+    SortedSet(card1, card2, card3, card4, card5).toList.reverse
   } catch {
     case e: NullPointerException => throw new CardIsNullException
   }
   if (cards.size != 5) throw new DuplicateCardsException
 
-  def highCard = cards.last
+  def highCard = cards.head
 }
 
 class InvalidHandException(msg:String) extends RuntimeException(msg)
