aba49d81164d31ba5dd0341c9519d71110e333bb
Gabriel Klappenbach <gabriel.klappenbach@gmail.com>
2012-03-14 22:04:04 +0100

    added class and implicit conversion from List to CardList, decorating the List[Card] class to be able to remove rank groups of certain sizes and returning the resulting list Added some checks to tests, even though it doesn't look so good right now.

--- src/main/scala/Hand.scala
+++ src/main/scala/Hand.scala
@@ -10,34 +10,37 @@ case class Hand(card1: Card, card2: Card, card3: Card, card4: Card, card5: Card)
 
   lazy val highCard = cards.reverse.head
 
-  lazy val isPair = removeRankGroupOfSize(cards, 2).size == 3
+  lazy val isPair = cards.removeRankGroupOfSize(2).size == 3
 
-  lazy val isTwoPair = removeRankGroupOfSize(cards, 2).size == 2
+  lazy val isTwoPair = cards.removeRankGroupOfSize(2).size == 2
 
   lazy val isStraight = cards.map(_.rank.id).toStream zip Stream.from(cards.head.rank.id) forall {
     case (a, b) => a == b
   }
 
-  lazy val isFullHouse = removeRankGroupOfSize( removeRankGroupOfSize(cards, 2), 3).size == 0
+  lazy val isFullHouse = cards.removeRankGroupOfSize(2).removeRankGroupOfSize(3).size == 0
 
   lazy val isFlush = cards.forall(card => card.suite.id == cards.head.suite.id )
 
   lazy val isStraightFlush = isStraight && isFlush
 
-  lazy val isFourOfAKind = removeRankGroupOfSize(cards, 4).size == 1  // 4 cards -> 1 since they are of equal rank
+  lazy val isFourOfAKind = cards.removeRankGroupOfSize(4).size == 1  // 4 cards -> 1 since they are of equal rank
 
   lazy val isThreeOfAKind = (cards map(_.rank.id) toSet).size == 3 // 3 cards -> 1 since they are of equal rank
 
-
-// TODO make it chainable, implicit con? binary op?
-
-  // Removes groups (grouped on rank, e.g. 7's or Jacks) of cards of a given group size
-  private def removeRankGroupOfSize(cards:List[Card], groupSize: Int): List[Card] = {
-    val groupedByRank = cards.groupBy(_.rank.id).values
-    val withoutGroupOfSize = groupedByRank.filterNot(_.size == groupSize)
-    withoutGroupOfSize.flatten.toList
+  trait CardList {
+    val cards: List[Card]
+    def removeRankGroupOfSize(groupSize: Int): List[Card] = {
+        val groupedByRank = cards.groupBy(_.rank.id).values
+        val withoutGroupOfSize = groupedByRank.filterNot(_.size == groupSize)
+        withoutGroupOfSize.flatten.toList
+      }
   }
+
+  implicit def toCardList(list: List[Card]): CardList = new CardList{ val cards = list}
 }
+
+
 
 class InvalidHandException(msg:String) extends RuntimeException(msg)
 class CardIsNullException extends InvalidHandException("No card in a hand may be null")
--- src/test/scala/HandTest.scala
+++ src/test/scala/HandTest.scala
@@ -34,6 +34,14 @@ class HandTest extends FunSuite with ShouldMatchers {
   test("2 3 4 5 6 in different suits forms a straight") {
     val hand = Hand(Card(TWO, HEARTS), Card(THREE, HEARTS), Card(FOUR, SPADES), Card(FIVE, SPADES), Card(SIX, HEARTS))
     hand.isStraight should be(true)
+
+    hand.isPair should be(false)
+    hand.isTwoPair should be(false)
+    hand.isThreeOfAKind should be(false)
+    hand.isFourOfAKind should be(false)
+    hand.isFlush should be(false)
+    hand.isStraightFlush should be(false)
+    hand.isFullHouse should be(false)
   }
 
   test("2 3 4 5 ace in different suits does not form any hand") {
@@ -45,12 +53,20 @@ class HandTest extends FunSuite with ShouldMatchers {
     hand.isFourOfAKind should be(false)
     hand.isFlush should be(false)
     hand.isStraightFlush should be(false)
-   hand.isFullHouse should be(false)
+    hand.isFullHouse should be(false)
   }
 
   test("2 3 4 6 ace of clubs forms a flush") {
     val hand = Hand(Card(TWO, CLUBS), Card(THREE, CLUBS), Card(FOUR, CLUBS), Card(SIX, CLUBS), Card(ACE, CLUBS))
     hand.isFlush should be(true)
+
+    hand.isStraight should be(false)
+    hand.isPair should be(false)
+    hand.isTwoPair should be(false)
+    hand.isThreeOfAKind should be(false)
+    hand.isFourOfAKind should be(false)
+    hand.isStraightFlush should be(false)
+    hand.isFullHouse should be(false)
   }
 
   test("2 of spades and 3 4 6 ace of clubs does not form a flush") {
@@ -61,11 +77,27 @@ class HandTest extends FunSuite with ShouldMatchers {
   test("2 3 4 5 6 of diamonds forms a straight flush") {
     val hand = Hand(Card(TWO, DIAMONDS), Card(THREE, DIAMONDS), Card(FOUR, DIAMONDS), Card(FIVE, DIAMONDS), Card(SIX, DIAMONDS))
     hand.isStraightFlush should be(true)
+
+    hand.isStraight should be(true)
+    hand.isPair should be(false)
+    hand.isTwoPair should be(false)
+    hand.isThreeOfAKind should be(false)
+    hand.isFourOfAKind should be(false)
+    hand.isFlush should be(true)
+    hand.isFullHouse should be(false)
   }
 
   test("5 5 5 5 6 of different suits forms four of a kind") {
     val hand = Hand(Card(FIVE, HEARTS), Card(FIVE, CLUBS), Card(FIVE, SPADES), Card(FIVE, DIAMONDS), Card(ACE, HEARTS))
     hand.isFourOfAKind should be(true)
+
+    hand.isStraight should be(false)
+    hand.isPair should be(false)
+    hand.isTwoPair should be(false)
+    hand.isThreeOfAKind should be(false)
+    hand.isFlush should be(false)
+    hand.isStraightFlush should be(false)
+    hand.isFullHouse should be(false)
   }
 
   test("5 5 5 2 6 of different suits does not form four of a kind") {
@@ -81,6 +113,14 @@ class HandTest extends FunSuite with ShouldMatchers {
   test("5 5 5 2 6 of different suits forms three of a kind") {
     val hand = Hand(Card(FIVE, HEARTS), Card(FIVE, CLUBS), Card(FIVE, SPADES), Card(TWO, DIAMONDS), Card(ACE, HEARTS))
     hand.isThreeOfAKind should be(true)
+
+    hand.isStraight should be(false)
+    hand.isPair should be(false)
+    hand.isTwoPair should be(false)
+    hand.isFourOfAKind should be(false)
+    hand.isFlush should be(false)
+    hand.isStraightFlush should be(false)
+    hand.isFullHouse should be(false)
   }
 
   test("5 5 5 5 6 of different suits does not form three of a kind") {
@@ -91,6 +131,13 @@ class HandTest extends FunSuite with ShouldMatchers {
   test("5 5 2 3 6 of different suits forms  a pair") {
     val hand = Hand(Card(FIVE, HEARTS), Card(FIVE, CLUBS), Card(TWO, SPADES), Card(THREE, DIAMONDS), Card(ACE, HEARTS))
     hand.isPair should be(true)
+
+    hand.isStraight should be(false)
+    hand.isThreeOfAKind should be(false)
+    hand.isFourOfAKind should be(false)
+    hand.isFlush should be(false)
+    hand.isStraightFlush should be(false)
+    hand.isFullHouse should be(false)
   }
 
   test("5 5 5 3 6 of different suits does not form  a pair") {
